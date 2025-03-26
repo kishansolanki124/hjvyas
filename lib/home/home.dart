adbb779ce909b65eeec7aa5bff6a6f96f5f4545c
Kishan Solanki <mr.kishansolanki@gmail.com>
2025-03-26 22:19:35 +0530

    Bottom nav setup started

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,17 +1,13 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home Page'),
-      ),
-      body: Center(
-        child: Text('Welcome to the Home Page!'),
-      ),
+      appBar: AppBar(title: Text('Home Page')),
+      body: Center(child: Text('Welcome to the Home Page!')),
     );
   }
 }
--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
-import '../home/home.dart';
+import '../home/navigation.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreen();
@@ -16,11 +16,14 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     // Start a timer that runs for 3 seconds
-    Timer(Duration(seconds: 3), () {
+    Timer(Duration(seconds: 1), () {
       // Navigate to the home page after 3 seconds
       Navigator.of(
         context,
-      ).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+        //).pushReplacement(MaterialPageRoute(builder: (context) => HomePage()));
+      ).pushReplacement(
+        MaterialPageRoute(builder: (context) => NavigationBarApp()),
+      );
     });
   }
 
@@ -41,75 +44,3 @@ class _SplashScreenState extends State<SplashScreen> {
     );
   }
 }
-
-// class SplashPage extends StatefulWidget {
-//   const SplashPage({super.key});
-//
-//   @override
-//   State<SplashPage> createState() => _SplashPageState();
-// }
-//
-// class _SplashPageState extends State<SplashPage> {
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       // appBar: AppBar(
-//       //   // TRY THIS: Try changing the color here to a specific color (to
-//       //   // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//       //   // change color while the other colors stay the same.
-//       //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//       //   // Here we take the value from the SplashPage object that was created by
-//       //   // the App.build method, and use it to set our appbar title.
-//       //   title: Text(""),
-//       // ),
-//       body: Container(
-//         child: Image.asset("images/bg.jpg"),
-//       ),
-//       );
-//   }
-// }
-
-//
-// class BackgroundImageWithLogo extends StatelessWidget {
-//   final String backgroundImagePath;
-//   final String logoImagePath;
-//   final double logoWidth; // Optional: Specify logo width
-//   final double logoHeight; // Optional: Specify logo height
-//
-//   BackgroundImageWithLogo({
-//     required this.backgroundImagePath,
-//     required this.logoImagePath,
-//     this.logoWidth = 150.0, // Default logo width
-//     this.logoHeight = 150.0, // Default logo height
-//   });
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Stack(
-//       fit: StackFit.expand, // Ensures the background image fills the screen
-//       children: <Widget>[
-//         // Background Image
-//         Image.asset(
-//           backgroundImagePath,
-//           fit: BoxFit.cover, // Cover the entire screen
-//         ),
-//
-//         // Centered Logo
-//         Center(
-//           child: Image.asset(
-//             logoImagePath,
-//             width: logoWidth,
-//             height: logoHeight,
-//           ),
-//         ),
-//       ],
-//     );
-//   }
-// }
