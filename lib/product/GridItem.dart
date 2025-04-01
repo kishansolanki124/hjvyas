d1675c69996288a33936fc559c89e5bf6282d56d
Kishan Solanki <mr.kishansolanki@gmail.com>
2025-04-01 20:38:16 +0530

    product listing page initiated

--- lib/product/GridItem.dart
+++ lib/product/GridItem.dart
@@ -14,42 +14,55 @@ class GridItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.blueGrey,
+      //color: Colors.blueGrey,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Image.network(
-            imageUrl,
-            height: 250, // height: 100, // Adjust height as needed
-            // width: 100,
-            fit: BoxFit.fill,
+          Card( // with Card
+            child: Image.asset("images/circular_demo.png"),
+            elevation: 10.0,
+            shape: const CircleBorder(),
+            clipBehavior: Clip.antiAlias,
           ),
+
+          // Image.network(
+          //   imageUrl,
+          //   height: 250, // height: 100, // Adjust height as needed
+          //   // width: 100,
+          //   fit: BoxFit.fill,
+          // ),
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: Text(
               title,
-              style: TextStyle(fontSize: 18,),
+              style: TextStyle(color: Color.fromARGB(255, 255, 255, 254),fontSize: 18,),
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Text(
               description,
-              style: TextStyle(fontSize: 14),
+              style: TextStyle(
+                color: Color.fromARGB(255, 2, 2, 2),
+                  fontSize: 14),
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Text(
               "Product life: 300 days",
-              style: TextStyle(fontSize: 14),
+              style: TextStyle(
+                  color: Color.fromARGB(255, 138, 138, 138),
+                  fontSize: 14),
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 8.0),
             child: Text(
               "Calories: 470",
-              style: TextStyle(fontSize: 14),
+              style: TextStyle(
+                  color: Color.fromARGB(255, 138, 138, 138),
+                  fontSize: 14),
             ),
           ),
         ],
