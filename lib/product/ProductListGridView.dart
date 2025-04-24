fbe5c1ade9fc87b43255cfc97b4fb056bb579793
Kishan Solanki <mr.kishansolanki@gmail.com>
2025-04-24 19:11:52 +0530

    Design feedback changes on product ContactUs.dart page

--- lib/product/ProductListGridView.dart
+++ lib/product/ProductListGridView.dart
@@ -7,9 +7,9 @@ import '../api/models/CategoryListResponse.dart';
 import '../api/services/HJVyasApiService.dart';
 import '../injection_container.dart';
 import '../product_detail/ProductDetail.dart';
+import 'ProductGridFirstItem.dart';
 import 'ProductGridFourthItem.dart';
 import 'ProductGridSecondItem.dart';
-import 'ProductGridFirstItem.dart';
 import 'ProductGridThirdItem.dart';
 
 class ProductListGridView extends StatefulWidget {
@@ -50,77 +50,80 @@ class _ProductListGridViewState extends State<ProductListGridView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Obx(() {
-          if (widget.paginationController.items.isEmpty &&
-              widget.paginationController.isLoading.value) {
-            return Center(child: CircularProgressIndicator());
-          }
+      body: Obx(() {
+        if (widget.paginationController.items.isEmpty &&
+            widget.paginationController.isLoading.value) {
+          //todo: change this
+          return Container(
+            color: Color.fromARGB(255, 31, 47, 80),
+            child: Center(child: CircularProgressIndicator()),
+          );
+        }
 
-          return NotificationListener<ScrollNotification>(
-            onNotification: (notification) {
-              if (notification is ScrollEndNotification &&
-                  notification.metrics.pixels ==
-                      notification.metrics.maxScrollExtent) {
-                widget.paginationController.loadMore(
-                  int.parse(widget.categoryListItem.categoryId),
-                );
-              }
-              return false;
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("images/bg.jpg"),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: Column(
-                children: <Widget>[
-                  // A non-scrolling widget at the top
-                  productListTopView(widget.categoryListItem.categoryName),
-                  // Use Expanded or Flexible to give the CustomScrollView a portion of the space
-                  Expanded(
-                    child: CustomScrollView(
-                      slivers: [
-                        SliverGrid(
-                          gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2, // 2 columns
-                                childAspectRatio:
-                                    (1 / 1.9), // Adjust item aspect ratio
-                              ),
-                          delegate: SliverChildBuilderDelegate(
-                            (context, index) {
-                              return productListItem(
-                                widget.paginationController.items[index],
-                                index,
-                                _navigateToDetails,
-                              );
-                            },
-                            childCount:
-                                widget.paginationController.items.length,
-                          ),
-                        ),
-                        SliverToBoxAdapter(
-                          child:
-                              widget.paginationController.isLoading.value
-                                  ? Padding(
-                                    padding: EdgeInsets.all(16.0),
-                                    child: Center(
-                                      child: CircularProgressIndicator(),
-                                    ),
-                                  )
-                                  : SizedBox.shrink(),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
+        return NotificationListener<ScrollNotification>(
+          onNotification: (notification) {
+            if (notification is ScrollEndNotification &&
+                notification.metrics.pixels ==
+                    notification.metrics.maxScrollExtent) {
+              widget.paginationController.loadMore(
+                int.parse(widget.categoryListItem.categoryId),
+              );
+            }
+            return false;
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("images/bg.jpg"),
+                fit: BoxFit.cover,
               ),
             ),
-          );
-        }),
-      );
+            child: Column(
+              children: <Widget>[
+                // A non-scrolling widget at the top
+                productListTopView(widget.categoryListItem.categoryName),
+                // Use Expanded or Flexible to give the CustomScrollView a portion of the space
+                Expanded(
+                  child: CustomScrollView(
+                    slivers: [
+                      SliverGrid(
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2, // 2 columns
+                              childAspectRatio:
+                                  (1 / 1.9), // Adjust item aspect ratio
+                            ),
+                        delegate: SliverChildBuilderDelegate(
+                          (context, index) {
+                            return productListItem(
+                              widget.paginationController.items[index],
+                              index,
+                              _navigateToDetails,
+                            );
+                          },
+                          childCount: widget.paginationController.items.length,
+                        ),
+                      ),
+                      SliverToBoxAdapter(
+                        child:
+                            widget.paginationController.isLoading.value
+                                ? Padding(
+                                  padding: EdgeInsets.all(16.0),
+                                  child: Center(
+                                    child: CircularProgressIndicator(),
+                                  ),
+                                )
+                                : SizedBox.shrink(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      }),
+    );
   }
 }
 
