3eed67d06c9a5f3f5f4891b452a34c162be2d924
Kishan Solanki <mr.kishansolanki@gmail.com>
2025-05-05 12:52:04 +0530

    hide bottom nav on scroll with animation for 1. combo page 2. product list page

--- lib/Test.dart
+++ lib/Test.dart
@@ -5,233 +5,102 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Persistent FAB Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const HomeWidget(),
+      title: 'FAB Slide Animation',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
     );
   }
 }
 
-class HomeWidget extends StatefulWidget {
-  const HomeWidget({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  State<HomeWidget> createState() => _HomeWidgetState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _HomeWidgetState extends State<HomeWidget> {
-  // Index to track which widget is currently displayed
-  int _currentIndex = 0;
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
+  bool _isVisible = true;
 
-  // List of widgets to display in the body
-  final List<Widget> _widgets = [const Widget1(), const Widget2()];
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
 
-  void _navigateToWidget(int index) {
+    _animation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0, 2), // Move down by 2x its height
+    ).animate(CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeInOut,
+    ));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _toggleFAB() {
     setState(() {
-      _currentIndex = index;
+      if (_isVisible) {
+        _controller.forward();
+      } else {
+        _controller.reverse();
+      }
+      _isVisible = !_isVisible;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Home Widget')),
-      // Use the current widget based on the index
-      body: _widgets[_currentIndex],
-      // The FloatingActionButton stays visible regardless of which widget is shown
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(const SnackBar(content: Text('Home FAB Pressed!')));
-        },
-        child: const Text('Button'),
+      appBar: AppBar(
+        title: const Text('FAB Slide Animation'),
+        centerTitle: true,
       ),
-    );
-  }
-}
-
-class Widget1 extends StatelessWidget {
-  const Widget1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Notice we don't use Scaffold here
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Widget First'),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // Get the _HomeWidgetState and call its navigation method
-              final homeState =
-                  context.findAncestorStateOfType<_HomeWidgetState>();
-              homeState?._navigateToWidget(1);
-            },
-            child: const Text('Navigate to Widget 2'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Widget2 extends StatelessWidget {
-  const Widget2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    // Notice we don't use Scaffold here either
-    return PopScope(
-      // Handle the back button press using the current recommended API
-      canPop: false,
-      onPopInvokedWithResult: (didPop, result) {
-        if (didPop) return;
-        final homeState = context.findAncestorStateOfType<_HomeWidgetState>();
-        homeState?._navigateToWidget(0);
-        //return true;
-      },
-      child: Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Widget 2'),
+            Text(
+              'FAB is ${_isVisible ? 'visible' : 'hidden'}',
+              style: const TextStyle(fontSize: 20),
+            ),
             const SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Get the _HomeWidgetState and call its navigation method
-                final homeState =
-                    context.findAncestorStateOfType<_HomeWidgetState>();
-                homeState?._navigateToWidget(0);
-              },
-              child: const Text('Back to Widget 1'),
+              onPressed: _toggleFAB,
+              child: Text(_isVisible ? 'Hide FAB' : 'Show FAB'),
             ),
           ],
         ),
       ),
-    );
-  }
-}
-
-// Alternative approach using Navigator.push
-// If you need to use traditional navigation with Navigator:
-
-class AlternativeHomeWidget extends StatelessWidget {
-  const AlternativeHomeWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Home Widget')),
-      body: const Widget1Alt(),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(const SnackBar(content: Text('Home FAB Pressed!')));
-        },
-        child: const Text('Button'),
-      ),
-    );
-  }
-}
-
-class Widget1Alt extends StatelessWidget {
-  const Widget1Alt({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Widget 1'),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: () {
-              // Use transparent page route to keep the home scaffold visible
-              Navigator.of(context).push(
-                _TransparentRoute(builder: (context) => const Widget2Alt()),
-              );
-            },
-            child: const Text('Navigate to Widget 2'),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Widget2Alt extends StatelessWidget {
-  const Widget2Alt({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
-      onPopInvokedWithResult: (didPop, result) {
-        if (didPop) return;
-        Navigator.of(context).pop();
-      },
-      child: Container(
-        color: Colors.white, // Background color to cover Widget1
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text('Widget 2'),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop();
-                },
-                child: const Text('Back to Widget 1'),
-              ),
-            ],
-          ),
+      floatingActionButton: SlideTransition(
+        position: _animation,
+        child: FloatingActionButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              const SnackBar(content: Text('FAB pressed!')),
+            );
+          },
+          child: const Icon(Icons.add),
         ),
       ),
     );
-  }
-}
-
-// Custom route that doesn't create a new scaffold
-class _TransparentRoute extends PageRoute<void> {
-  _TransparentRoute({required this.builder}) : super(fullscreenDialog: false);
-
-  final WidgetBuilder builder;
-
-  @override
-  bool get opaque => false;
-
-  @override
-  bool get barrierDismissible => false;
-
-  @override
-  Color? get barrierColor => null;
-
-  @override
-  String? get barrierLabel => null;
-
-  @override
-  bool get maintainState => true;
-
-  @override
-  Duration get transitionDuration => const Duration(milliseconds: 300);
-
-  @override
-  Widget buildPage(
-    BuildContext context,
-    Animation<double> animation,
-    Animation<double> secondaryAnimation,
-  ) {
-    return builder(context);
   }
 }
--- lib/home/navigation.dart
+++ lib/home/navigation.dart
@@ -51,13 +51,19 @@ class _NavigationExampleState extends State<NavigationExample>
     with TickerProviderStateMixin {
   int currentPageIndex = 0;
   bool _showBottomNavBar = true; //BottomNavigationBar visibility
+  late AnimationController _controller;
+  late Animation<Offset> _animation;
 
   // This method will be called by ScrollWidget when the user scrolls
   void _updateBottomNavBarVisibility(bool show) {
     if (mounted) {
-      // //check if the widget is mounted before calling setState
       setState(() {
-        _showBottomNavBar = show;
+        if (_showBottomNavBar) {
+          _controller.forward();
+        } else {
+          _controller.reverse();
+        }
+        _showBottomNavBar = !_showBottomNavBar;
       });
     }
   }
@@ -92,12 +98,11 @@ class _NavigationExampleState extends State<NavigationExample>
     CategoryListItem categoryListItem,
     String logoURL,
   ) {
-
     setState(() {
       homeNavigationWidget = ProductListGridView(
         categoryListItem: categoryListItem,
         logoURL: logoURL,
-          updateBottomNavBarVisibility: _updateBottomNavBarVisibility,
+        updateBottomNavBarVisibility: _updateBottomNavBarVisibility,
       );
     });
   }
@@ -109,117 +114,119 @@ class _NavigationExampleState extends State<NavigationExample>
   }
 
   @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+
+    _animation = Tween<Offset>(
+      begin: Offset.zero,
+      end: const Offset(0, 2), // Move down by 2x its height
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     //final ThemeData theme = Theme.of(context);
     return Scaffold(
       backgroundColor: Color.fromARGB(255, 32, 47, 80),
       body: homeNavigationWidget,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.fromLTRB(20.0, 0, 10.0, 0),
-        child:
-            _showBottomNavBar
-                ? DecoratedBox(
-                  decoration: BoxDecoration(
-                    color: Color.fromARGB(225, 255, 255, 255),
-                    borderRadius: BorderRadius.circular(20),
+      floatingActionButton: SlideTransition(
+        position: _animation,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20.0, 0, 10.0, 0),
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              color: Color.fromARGB(225, 255, 255, 255),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: BottomNavigationBar(
+              unselectedLabelStyle: TextStyle(
+                fontSize: 10.0,
+                fontFamily: "Montserrat",
+                color: Colors.black,
+                fontWeight: FontWeight.w700,
+              ),
+              selectedLabelStyle: TextStyle(
+                fontSize: 10.0,
+                fontFamily: "Montserrat",
+                color: Color.fromARGB(255, 101, 115, 169),
+                fontWeight: FontWeight.w700,
+              ),
+              type: BottomNavigationBarType.fixed,
+              iconSize: 12.0,
+              selectedFontSize: 10.0,
+              unselectedFontSize: 10.0,
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              items: <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: bottomNavIcon("icons/home_icon.png"),
+                  activeIcon: bottomNavIcon("icons/home_icon_a.png"),
+                  label: "HOME",
+                ),
+                BottomNavigationBarItem(
+                  icon: bottomNavIcon("icons/product_icon.png"),
+                  activeIcon: bottomNavIcon("icons/product_icon_a.png"),
+                  label: "PROUDUCTS",
+                ),
+                BottomNavigationBarItem(
+                  icon: bottomNavIcon("icons/combo_icon.png"),
+                  activeIcon: bottomNavIcon("icons/combo_icon_a.png"),
+                  label: "COMBO",
+                ),
+                BottomNavigationBarItem(
+                  icon: Badge(
+                    largeSize: 16,
+                    backgroundColor: Colors.red,
+                    label: Text(
+                      "1",
+                      style: TextStyle(
+                        fontSize: 10,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: "Montserrat",
+                      ),
+                    ),
+                    textStyle: TextStyle(fontSize: 16),
+                    child: bottomNavIcon("icons/my_bag_icon.png"),
                   ),
-                  child:
-                      _showBottomNavBar
-                          ? BottomNavigationBar(
-                            unselectedLabelStyle: TextStyle(
-                              fontSize: 10.0,
-                              fontFamily: "Montserrat",
-                              color: Colors.black,
-                              fontWeight: FontWeight.w700,
-                            ),
-                            selectedLabelStyle: TextStyle(
-                              fontSize: 10.0,
-                              fontFamily: "Montserrat",
-                              color: Color.fromARGB(255, 101, 115, 169),
-                              fontWeight: FontWeight.w700,
-                            ),
-                            type: BottomNavigationBarType.fixed,
-                            iconSize: 12.0,
-                            selectedFontSize: 10.0,
-                            unselectedFontSize: 10.0,
-                            elevation: 0,
-                            backgroundColor: Colors.transparent,
-                            items: <BottomNavigationBarItem>[
-                              BottomNavigationBarItem(
-                                icon: bottomNavIcon("icons/home_icon.png"),
-                                activeIcon: bottomNavIcon(
-                                  "icons/home_icon_a.png",
-                                ),
-                                label: "HOME",
-                              ),
-                              BottomNavigationBarItem(
-                                icon: bottomNavIcon("icons/product_icon.png"),
-                                activeIcon: bottomNavIcon(
-                                  "icons/product_icon_a.png",
-                                ),
-                                label: "PROUDUCTS",
-                              ),
-                              BottomNavigationBarItem(
-                                icon: bottomNavIcon("icons/combo_icon.png"),
-                                activeIcon: bottomNavIcon(
-                                  "icons/combo_icon_a.png",
-                                ),
-                                label: "COMBO",
-                              ),
-                              BottomNavigationBarItem(
-                                icon: Badge(
-                                  largeSize: 16,
-                                  backgroundColor: Colors.red,
-                                  label: Text(
-                                    "1",
-                                    style: TextStyle(
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.w700,
-                                      fontFamily: "Montserrat",
-                                    ),
-                                  ),
-                                  textStyle: TextStyle(fontSize: 16),
-                                  child: bottomNavIcon("icons/my_bag_icon.png"),
-                                ),
-                                activeIcon: Badge(
-                                  largeSize: 20,
-                                  backgroundColor: Colors.red,
-                                  label: Text(
-                                    "1",
-                                    style: TextStyle(
-                                      fontSize: 12,
-                                      fontWeight: FontWeight.w700,
-                                      fontFamily: "Montserrat",
-                                    ),
-                                  ),
-                                  textStyle: TextStyle(fontSize: 16),
-                                  child: bottomNavIcon(
-                                    "icons/my_bag_icon_a.png",
-                                  ),
-                                ),
-                                label: "MY BAG",
-                              ),
-                              BottomNavigationBarItem(
-                                icon: bottomNavIcon("icons/about_icon.png"),
-                                activeIcon: bottomNavIcon(
-                                  "icons/about_icon_a.png",
-                                ),
-                                label: "ABOUT",
-                              ),
-                            ],
-                            currentIndex: currentPageIndex,
-                            selectedItemColor: Color.fromARGB(
-                              255,
-                              101,
-                              115,
-                              169,
-                            ),
-                            onTap: _onItemTapped,
-                          )
-                          : null,
-                )
-                : null,
+                  activeIcon: Badge(
+                    largeSize: 20,
+                    backgroundColor: Colors.red,
+                    label: Text(
+                      "1",
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: "Montserrat",
+                      ),
+                    ),
+                    textStyle: TextStyle(fontSize: 16),
+                    child: bottomNavIcon("icons/my_bag_icon_a.png"),
+                  ),
+                  label: "MY BAG",
+                ),
+                BottomNavigationBarItem(
+                  icon: bottomNavIcon("icons/about_icon.png"),
+                  activeIcon: bottomNavIcon("icons/about_icon_a.png"),
+                  label: "ABOUT",
+                ),
+              ],
+              currentIndex: currentPageIndex,
+              selectedItemColor: Color.fromARGB(255, 101, 115, 169),
+              onTap: _onItemTapped,
+            ),
+          ),
+        ),
       ),
     );
   }
